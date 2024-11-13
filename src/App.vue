e2c568b97b2f93ed5999db2c030065e1a6ca3637
hocnguyen12 <paul.nguyen56545@gmail.com>
2024-11-13 10:49:03 +0100

    merged

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,22 @@
 
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link>
-      <router-link to="/profile">Profile</router-link>
-      <router-link to="/map">Map</router-link>
+    <nav class="bg-white text-gray-800 shadow-md p-4 fixed top-0 left-0 w-full z-10">
+      <div class="flex items-center justify-between">
+        <div class="text-lg font-semibold">NewP</div>
+        <div class="space-x-6">
+          <a href="/" class="hover:text-gray-500">Home</a>
+          <a href="/map" class="hover:text-gray-500">Activity Map</a>
+          <a href="/about" class="hover:text-gray-500">About</a>
+        </div>
+      </div>
     </nav>
-    <router-view></router-view>
+    <div class="pt-8"> <!-- Espace sous la navbar pour ne pas cacher le contenu -->
+      <router-view />
+    </div>
   </div>
 </template>
+
 
 <style scoped>
 .logo {
--- src/views/MapPage.vue
+++ src/views/MapPage.vue
@@ -1,9 +1,26 @@
 <template>
+  <div>
+    <h1>Map Page</h1>
+    <div id="map" style="height: 500px; width: 100%;"></div>
+  </div>
 </template>
 
 
 <script>
-  export default {
-    name: 'MapPage'
-  };
+import { onMounted } from 'vue';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+export default {
+  name: 'MapPage',
+  setup() {
+    onMounted(() => {
+        var map = L.map('map').setView([48.8566, 2.3522], 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '© OpenStreetMap contributors',
+        }).addTo(map);
+    });
+  }
+};
 </script>
